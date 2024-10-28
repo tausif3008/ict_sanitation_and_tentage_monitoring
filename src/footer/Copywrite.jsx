a60cb2800c7e81522bac378673bb70246813b0b1
sachin123jadhav <sachin123jadhav@gmail.com>
2024-10-28 12:43:52 +0530

    UI changes header footer

--- src/footer/Copywrite.jsx
+++ src/footer/Copywrite.jsx
@@ -5,15 +5,27 @@ const CopyWrite = ({ lang = "en" }) => {
   const dict = DICT; // Accessing your dictionary for language support.
 
   return (
-    <footer className="h-12 w-full flex items-center text-center justify-center bg-orange-400 text-white font-semibold font-roboto">
-      <div className="md:w-10/12 w-9/12 xl:w-10/12 2xl:w-9/12 flex flex-col items-center justify-center m-auto">
-        {/* Optional multi-language footer */}
-        {/* {dict.last_footer[lang]} */}
-        <span>{dict.copy_right[lang]}</span>
-        <small className="text-xs mt-1">{dict.hosted_by[lang]}</small>{" "}
-        {/* Smaller text */}
+    <>
+      <div class="flex bg-orange-400 text-white px-3 py-2">
+        <div class="flex-1 text-left">
+          <span>{dict.copy_right[lang]}</span>
+        </div>
+        <div class="flex-1 text-right">
+          {" "}
+          <small className="text-xs mt-1">{dict.hosted_by[lang]}</small>{" "}
+        </div>
       </div>
-    </footer>
+
+      {/* <footer className="h-12 w-full flex  justify-center bg-orange-400 text-white font-semibold font-roboto">
+        <div className="md:w-12/12 w-9/12 xl:w-12/12 2xl:w-/12 flex flex-col  justify-center m-auto"> */}
+          {/* Optional multi-language footer */}
+          {/* {dict.last_footer[lang]} */}
+
+          {/* Smaller text */}
+        {/* </div>
+      
+      </footer> */}
+    </>
   );
 };
 
