c2aad34b0a7902eda95454a664cc710c2a302ef7
Rohitsalunke1216 <rohitsalunke1216@gmail.com>
2024-11-13 17:50:13 +0530

    create excel file and pdf

--- src/Reports/VendorwiseReports/index.jsx
+++ src/Reports/VendorwiseReports/index.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 import * as XLSX from "xlsx";
-import { Table, Typography, Button, Space, message } from "antd";
+import { Button, Space, message } from "antd";
 
 import CommonDivider from "../../commonComponents/CommonDivider";
 import URLS from "../../urils/URLS";
@@ -25,9 +25,6 @@ const VendorReports = () => {
     pageLength: 25,
     currentPage: 1,
   });
-
-  // console.log("vendorReports", vendorReports);
-  console.log("excelData", excelData);
 
   useEffect(() => {
     if (vendorReports) {
@@ -93,6 +90,7 @@ const VendorReports = () => {
     },
   ];
 
+  // excel
   const exportToExcel = () => {
     if (excelData && excelData?.length > 0) {
       const worksheet = XLSX.utils.json_to_sheet(excelData);
@@ -104,105 +102,11 @@ const VendorReports = () => {
     }
   };
 
-  // const exportToPDF = () => {
-  //   message.success("Downloading pdf it get some time");
-  //   const doc = new jsPDF("landscape");
-
-  //   // Centered ICT heading
-  //   const ictHeading = "ICT Sanitation and Tentage Monitoring System";
-  //   const pageWidth = doc.internal.pageSize.getWidth();
-  //   const ictX = (pageWidth - doc.getTextWidth(ictHeading)) / 2; // Center the heading
-  //   doc.setFontSize(14);
-  //   doc.setFont("bold");
-  //   doc.text(ictHeading, ictX, 10); // Heading position
-
-  //   // Image on the Left (Company Logo or similar image)
-  //   const leftImageX = 10; // X position (from the left)
-  //   const leftImageY = 10; // Y position (from the top)
-  //   const leftImageWidth = 30; // Image width (adjust as needed)
-  //   const leftImageHeight = 25; // Image height (adjust as needed)
-  //   doc.addImage(
-  //     `${IMAGELIST?.kumbh}`,
-  //     "PNG",
-  //     leftImageX,
-  //     leftImageY,
-  //     leftImageWidth,
-  //     leftImageHeight
-  //   );
-
-  //   // Image on the Right (Another logo or image)
-  //   const rightImageX = pageWidth - 40; // X position (from the right)
-  //   const rightImageY = 10; // Y position (from the top)
-  //   const rightImageWidth = 30; // Image width (adjust as needed)
-  //   const rightImageHeight = 25; // Image height (adjust as needed)
-  //   doc.addImage(
-  //     `${IMAGELIST?.govt_logo}`,
-  //     "PNG",
-  //     rightImageX,
-  //     rightImageY,
-  //     rightImageWidth,
-  //     rightImageHeight
-  //   );
-
-  //   // Add report title and date on the same line
-  //   const title = "Vendor-Wise Report";
-  //   const date = new Date();
-  //   const dateString = date.toLocaleString(); // Format the date and time
-
-  //   // Calculate positions
-  //   const titleX = 14; // Left align title
-  //   const dateX = pageWidth - doc.getTextWidth(dateString) - 14; // 14 units from the right
-
-  //   // Add title and date
-  //   doc.setFontSize(12);
-  //   doc.setFont("bold");
-  //   doc.text(title, titleX, 25); // Title position
-  //   doc.setFont("normal");
-  //   doc.setFontSize(10); // Smaller font size for date
-  //   doc.text(dateString, dateX, 25); // Date position
-
-  //   // Add a horizontal line below the header
-  //   doc.line(10, 30, 200, 30); // x1, y1, x2, y2
-
-  //   // Table header and content
-  //   doc.autoTable({
-  //     head: [
-  //       [
-  //         "Vendor Name",
-  //         "Email",
-  //         "Phone",
-  //         "Address",
-  //         "Pin code",
-  //         "Company",
-  //         "Language",
-  //         "",
-  //       ],
-  //     ],
-  //     body: excelData.map((opt) => [
-  //       opt?.name,
-  //       opt?.email,
-  //       opt?.phone,
-  //       opt?.address,
-  //       opt?.pin,
-  //       opt?.company,
-  //       opt?.language,
-  //     ]),
-  //     startY: 35, // Start after the header
-  //   });
-
-  //   // Add footer
-  //   const footerText1 = "Maha Kumbh Mela 2025, Prayagraj Mela Authority.";
-  //   const footerX = (pageWidth - doc.getTextWidth(footerText1)) / 2; // Center footer
-  //   const footerY = doc.internal.pageSize.getHeight() - 20; // 20 units from the bottom
-
-  //   doc.setFontSize(10);
-  //   doc.text(footerText1, footerX, footerY + 5); // Adjust for footer spacing
-
-  //   doc.save("Vendor-Wise-Report.pdf");
-  // };
-
+  // pdf
   const exportToPDF = () => {
-    const doc = new jsPDF("landscape");
+    const doc = new jsPDF("landscape", undefined, undefined, {
+      compress: true,
+    });
 
     // Centered ICT heading
     const ictHeading = "ICT Sanitation and Tentage Monitoring System";
@@ -212,32 +116,38 @@ const VendorReports = () => {
     doc.setFont("bold");
     doc.text(ictHeading, ictX, 10); // Heading position
 
-    // Image on the Left (Company Logo or similar image)
+    // // Image on the Left (Company Logo or similar image)
     const leftImageX = 10; // X position (from the left)
     const leftImageY = 10; // Y position (from the top)
     const leftImageWidth = 30; // Image width (adjust as needed)
     const leftImageHeight = 25; // Image height (adjust as needed)
     doc.addImage(
       `${IMAGELIST?.kumbh}`,
-      "PNG",
+      "JPEG",
       leftImageX,
       leftImageY,
       leftImageWidth,
-      leftImageHeight
+      leftImageHeight,
+      undefined,
+      undefined,
+      "FAST" // Adds compression for smaller file size
     );
 
-    // Image on the Right (Another logo or image)
+    // // Image on the Right (Another logo or image)
     const rightImageX = pageWidth - 40; // X position (from the right)
     const rightImageY = 10; // Y position (from the top)
     const rightImageWidth = 30; // Image width (adjust as needed)
     const rightImageHeight = 25; // Image height (adjust as needed)
     doc.addImage(
       `${IMAGELIST?.govt_logo}`,
-      "PNG",
+      "JPEG",
       rightImageX,
       rightImageY,
       rightImageWidth,
-      rightImageHeight
+      rightImageHeight,
+      undefined,
+      undefined,
+      "FAST" // Adds compression for smaller file size
     );
 
     // Add report title and date on the same line
@@ -304,14 +214,28 @@ const VendorReports = () => {
     <div>
       <CommonDivider label={"Vendor-Wise Report"} />
       <Space style={{ marginBottom: 16, float: "right" }}>
-        <Button type="primary" onClick={exportToExcel}>
+        <Button
+          type="primary"
+          onClick={() => {
+            if (excelData && excelData?.length > 0) {
+              message.success("Downloading excel, it might take some time...");
+              exportToExcel();
+            } else {
+              message.error("Data is not available.");
+            }
+          }}
+        >
           Download Excel
         </Button>
         <Button
           type="primary"
           onClick={() => {
-            message.success("Downloading pdf, it might take some time...");
-            exportToPDF();
+            if (excelData && excelData?.length > 0) {
+              message.success("Downloading pdf, it might take some time...");
+              exportToPDF();
+            } else {
+              message.error("Data is not available.");
+            }
           }}
         >
           Download PDF
