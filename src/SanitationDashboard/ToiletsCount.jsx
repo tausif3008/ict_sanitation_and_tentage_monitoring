08090c5a2c73fe370f599aa6b6f6bf2d91fe6536
Rohitsalunke1216 <rohitsalunke1216@gmail.com>
2024-11-22 18:28:11 +0530

    add change in sanitain dash for view more

--- src/SanitationDashboard/ToiletsCount.jsx
+++ src/SanitationDashboard/ToiletsCount.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useOutletContext } from "react-router";
+import { useDispatch } from "react-redux";
 import {
   CheckCircleOutlined,
   SyncOutlined,
@@ -11,11 +12,26 @@ import card_orange from "../assets/Dashboard/card_orange.png";
 import card_red from "../assets/Dashboard/card_red.png";
 import card_purple from "../assets/Dashboard/card_purple.png";
 import SanitationDashSelector from "./Slice/sanitationDashboardSelector";
+import ViewTableData from "../commonComponents/View/ViewTableData";
+import URLS from "../urils/URLS";
+import { getAssetTypes } from "../register/AssetType/AssetTypeSlice";
+import AssetTypeSelectors from "../register/AssetType/assetTypeSelectors";
 
 const ToiletsCount = () => {
   const [dict, lang] = useOutletContext();
+  const [showTable, setShowTable] = useState(false);
+  const [showTableList, setTableList] = useState({ list: [] }); // vendor list
 
+  const dispatch = useDispatch();
   const { SanitationDash_data, loading } = SanitationDashSelector(); // sanitation dashboard ( api call in details page of vendor dashboard)
+  const { AssetType } = AssetTypeSelectors(); // asset type
+
+  useEffect(() => {
+    if (AssetType) {
+      setTableList({ list: AssetType.data.assettypes });
+    }
+  }, [AssetType]);
+
   const {
     off_monitoring = 0,
     under_monitoring = 0,
@@ -27,6 +43,32 @@ const ToiletsCount = () => {
     return new Intl.NumberFormat("en-IN").format(number);
   };
 
+  const handleTotal = async () => {
+    const url = URLS?.assetType?.path + 1;
+    dispatch(getAssetTypes(url)); // get assset type
+    setShowTable(true);
+  };
+
+  // close list
+  const handleCancel = () => {
+    setShowTable(false);
+  };
+
+  const tableColumn = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      // width: "10%",
+    },
+    {
+      title: "Total Quantity",
+      dataIndex: "total_quantity",
+      key: "total_quantity",
+      width: "20%",
+    },
+  ];
+
   return (
     <div className="p-3 mx-auto bg-white rounded-xl space-y-4">
       <div className="text-xl font-bold mb-4">
@@ -34,7 +76,10 @@ const ToiletsCount = () => {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-1 md:grid-cols-2 sm:grid-cols-2 gap-4">
-        <div className="relative p-3 border rounded-md shadow-md bg-blue-50">
+        <div
+          className="relative p-3 border rounded-md shadow-md bg-blue-50"
+          onClick={handleTotal}
+        >
           <div className="text-start">
             <div className="text-blue-600 font-semibold flex flex-col gap-2 items-start relative">
               <div className="flex items-center gap-2">
@@ -118,6 +163,15 @@ const ToiletsCount = () => {
           />
         </div>
       </div>
+
+      <ViewTableData
+        title={"Toilet List"}
+        openModal={showTable}
+        handleCancel={handleCancel}
+        tableData={showTableList || []}
+        column={tableColumn || []}
+        // footer={`Total Allotted Quantity : ${allQuantity}`}
+      />
     </div>
   );
 };
