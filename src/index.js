b3fe36ae213c20e5321a6bda287552f8775ee80e
Rohitsalunke1216 <rohitsalunke1216@gmail.com>
2024-12-08 19:34:15 +0530

    add chage in route

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 import { ConfigProvider } from "antd";
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./Redux/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ConfigProvider
-    theme={{
-      components: {
-        Table: {
-          borderColor: "#b5f5ec",
-          headerBg: "orange",
-          cellPaddingBlock: 5,
-          headerColor: "white",
-          rowHoverBg: "#f6ffed",
-          headerBorderRadius: 0,
-        },
-      },
-    }}
-  >
-    <App /> {/* Your main App component */}
-  </ConfigProvider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ConfigProvider
+        theme={{
+          components: {
+            Table: {
+              borderColor: "#b5f5ec",
+              headerBg: "orange",
+              cellPaddingBlock: 5,
+              headerColor: "white",
+              rowHoverBg: "#f6ffed",
+              headerBorderRadius: 0,
+            },
+          },
+        }}
+      >
+        <App /> {/* Your main App component */}
+      </ConfigProvider>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
